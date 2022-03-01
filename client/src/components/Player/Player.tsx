d2fe3fc22b083d0e3bc7f679035fe4f8f6b4ad1b
Amokstakov <amoksyak@uoguelph.ca>
2022-02-28 23:20:31 -0800

    Finishing the player and slider

--- client/src/components/Player/Player.tsx
+++ client/src/components/Player/Player.tsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
 import YouTube from "react-youtube";
-import { Button } from "../Common/Button";
+import { FaPlayCircle, FaPauseCircle, FaYoutube } from "react-icons/fa";
+import { AiOutlineHeart } from "react-icons/ai";
+import { IconContext } from "react-icons";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 interface IPlayer {
   playVideo: () => void;
   pauseVideo: () => void;
+  setVolume: (volume: number | number[]) => void;
 }
 
 export const Player = () => {
   const [player, setPlayer] = useState<IPlayer>();
   // TODO: change this to be a static list fetched
   const [videoId, SetVideoId] = useState("0uw1Adx0psw");
+  const [playAudio, setPlayAudio] = useState(true);
 
   const onReady = (e: any) => {
     setPlayer(e.target);
@@ -23,15 +29,53 @@ export const Player = () => {
   const onPauseVideo = () => {
     player?.pauseVideo();
   };
+
+  const onVolumeChange = (value: number | number[]) => {
+    player?.setVolume(value);
+  };
+
+  const triggerAudio = () => {
+    if (playAudio) {
+      onPlayVideo();
+    } else {
+      onPauseVideo();
+    }
+
+    setPlayAudio(!playAudio);
+  };
   return (
     <>
-      <YouTube className="hidden" videoId={videoId} onReady={onReady} />
-      <Button onClick={onPlayVideo} disabled={!player}>
-        Play
-      </Button>
-      <Button onClick={onPauseVideo} disabled={!player}>
-        Pause
-      </Button>
+      <div className="py-4 px-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 w-1/2">
+        <div className="flex items-center space-x-6 justify-between">
+          <div>Name of Song</div>
+          <div className="flex space-x-2">
+            <IconContext.Provider value={{ size: "1.1rem" }}>
+              <FaYoutube />
+            </IconContext.Provider>
+            <IconContext.Provider value={{ size: "1.1rem" }}>
+              <AiOutlineHeart />
+            </IconContext.Provider>
+          </div>
+        </div>
+        <YouTube className="hidden" videoId={videoId} onReady={onReady} />
+        <div className="flex items-center space-x-3">
+          <IconContext.Provider value={{ size: "1.5rem" }}>
+            {playAudio ? (
+              <FaPlayCircle onClick={triggerAudio} />
+            ) : (
+              <FaPauseCircle onClick={triggerAudio} />
+            )}
+          </IconContext.Provider>
+          <Slider
+            defaultValue={75}
+            onChange={(value) => {
+              onVolumeChange(value);
+            }}
+            trackStyle={[{ backgroundColor: "black" }]}
+            handleStyle={[{ backgroundColor: "black", borderColor: "white" }]}
+          />
+        </div>
+      </div>
     </>
   );
 };
--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,13 +1,13 @@
 import { Button } from "../components/Common/Button";
 import { useSetBackground } from "../store";
 import { Player } from "../components/Player/Player";
+import { FaPlayCircle } from "react-icons/fa";
 
 export const HomePage = ({ backgrounds }: { backgrounds: any }) => {
   const { isBackground, setIsBackground } = useSetBackground();
   return (
     <div className="h-full w-full">
-      <div className="flex justify-end relative">
-        <Player />
+      <div className="flex justify-end">
         <Button onClick={() => setIsBackground(backgrounds.SPACE)}>
           Button
         </Button>
@@ -17,6 +17,9 @@ export const HomePage = ({ backgrounds }: { backgrounds: any }) => {
         <Button onClick={() => setIsBackground(backgrounds.CITY)}>
           Button
         </Button>
+      </div>
+      <div className="flex justify-center">
+        <Player />
       </div>
     </div>
   );
