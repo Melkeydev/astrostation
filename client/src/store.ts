681d79b99dc03b031628a200d495c5ebf664e2f3
Arnold Chand <creativenull@outlook.com>
2022-03-14 17:27:58 -0400

    feat: persist store on reloads

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,5 +1,11 @@
 import create from "zustand";
+import { persist } from "zustand/middleware";
 
+/**
+ * Task Store
+ * ---
+ * Handle the tasks created in the tasks section
+ */
 interface Task {
   id: number;
   description: string;
@@ -15,91 +21,148 @@ interface TaskState {
   completeTask: (id: number) => void;
 }
 
-export const useTask = create<TaskState>((set) => ({
-  // initial state
-  tasks: [],
-  // methods for manipulating state
-  addTask: (description: string) => {
-    set((state) => ({
-      tasks: [
-        ...state.tasks,
-        {
-          id: state.tasks.length + 1,
-          description,
-          inProgress: false,
-          completed: false,
-        } as Task,
-      ],
-    }));
-  },
-  removeTask: (id) => {
-    set((state) => ({
-      tasks: state.tasks.filter((task) => task.id !== id),
-    }));
-  },
-  toggleInProgressState: (id) => {
-    set((state) => ({
-      tasks: state.tasks.map((task) =>
-        task.id === id
-          ? ({ ...task, inProgress: !task.inProgress } as Task)
-          : task
-      ),
-    }));
-  },
-  completeTask: (id) => {
-    set((state) => ({
-      tasks: state.tasks.map((task) =>
-        task.id === id
-          ? ({ ...task, completed: !task.completed } as Task)
-          : task
-      ),
-    }));
-  },
-}));
+export const useTask = create<TaskState>(
+  persist(
+    (set, _) => ({
+      // initial state
+      tasks: [],
+      // methods for manipulating state
+      addTask: (description: string) => {
+        set((state) => ({
+          tasks: [
+            ...state.tasks,
+            {
+              id: state.tasks.length + 1,
+              description,
+              inProgress: false,
+              completed: false,
+            } as Task,
+          ],
+        }));
+      },
+      removeTask: (id) => {
+        set((state) => ({
+          tasks: state.tasks.filter((task) => task.id !== id),
+        }));
+      },
+      toggleInProgressState: (id) => {
+        set((state) => ({
+          tasks: state.tasks.map((task) =>
+            task.id === id
+              ? ({ ...task, inProgress: !task.inProgress } as Task)
+              : task
+          ),
+        }));
+      },
+      completeTask: (id) => {
+        set((state) => ({
+          tasks: state.tasks.map((task) =>
+            task.id === id
+              ? ({ ...task, completed: !task.completed } as Task)
+              : task
+          ),
+        }));
+      },
+    }),
+    {
+      name: "tasks",
+    }
+  )
+);
 
+/**
+ * Background Store
+ * ---
+ * Handles the background image state of app
+ */
 type IBackground = {
   isBackground: number;
   setIsBackground: (isBackground: number) => void;
 };
 
-export const useSetBackground = create<IBackground>((set) => ({
-  isBackground: 0,
-  setIsBackground: (isBackground) => set({ isBackground }),
-}));
+export const useSetBackground = create<IBackground>(
+  persist(
+    (set, _) => ({
+      isBackground: 0,
+      setIsBackground: (isBackground) => set({ isBackground }),
+    }),
+    {
+      name: "app_background",
+    }
+  )
+);
 
+/**
+ * Tasks Section Store
+ * ---
+ * Handle the visibility of the tasks section
+ */
 type IToggleTasks = {
   isTasksToggled: boolean;
   setIsTasksToggled: (isTasksToggled: boolean) => void;
 };
 
-export const useToggleTasks = create<IToggleTasks>((set) => ({
-  isTasksToggled: true,
-  setIsTasksToggled: (isTasksToggled) => set({ isTasksToggled }),
-}));
+export const useToggleTasks = create<IToggleTasks>(
+  persist(
+    (set, _) => ({
+      isTasksToggled: true,
+      setIsTasksToggled: (isTasksToggled) => set({ isTasksToggled }),
+    }),
+    {
+      name: "show_tasks_section",
+    }
+  )
+);
 
+/**
+ * Music Section Store
+ * ---
+ * Handle the visibility of the music section
+ */
 type IToggleMusic = {
   isMusicToggled: boolean;
   setIsMusicToggled: (isMusicToggled: boolean) => void;
 };
 
-export const useToggleMusic = create<IToggleMusic>((set) => ({
-  isMusicToggled: true,
-  setIsMusicToggled: (isMusicToggled) => set({ isMusicToggled }),
-}));
+export const useToggleMusic = create<IToggleMusic>(
+  persist(
+    (set, _) => ({
+      isMusicToggled: true,
+      setIsMusicToggled: (isMusicToggled) => set({ isMusicToggled }),
+    }),
+    {
+      name: "show_music_section",
+    }
+  )
+);
 
+/**
+ * Timer Section Store
+ * ---
+ * Handle the visibility of the timer section
+ */
 type IToggleTimer = {
   isTimerToggled: boolean;
   setIsTimerToggled: (isTimerToggled: boolean) => void;
 };
 
-export const useToggleTimer = create<IToggleTimer>((set) => ({
-  isTimerToggled: true,
-  setIsTimerToggled: (isTimerToggled) => set({ isTimerToggled }),
-}));
+export const useToggleTimer = create<IToggleTimer>(
+  persist(
+    (set, _) => ({
+      isTimerToggled: true,
+      setIsTimerToggled: (isTimerToggled) => set({ isTimerToggled }),
+    }),
+    { name: "show_timer_section" }
+  )
+);
 
-{
-  /*UNUSED*/
-}
+/**
+ * Side Nav Store
+ * ---
+ * Handle the visibility of the Sidebar Navigation section
+ */
+
+// TODO: Decide if we should use this or omit
 type IToggleSideNav = {
   isSideNavToggled: boolean;
   setIsSideNavToggled: (isSideNavToggledToggled: boolean) => void;
