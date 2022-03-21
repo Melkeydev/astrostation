669d5d096a839568a4a0ca99150c2fd233b9a573
Arnold Chand <creativenull@outlook.com>
2022-03-21 12:00:10 -0400

    fix: even spacing for station selector

--- client/src/components/Player/StationSelector.tsx
+++ client/src/components/Player/StationSelector.tsx
@@ -13,55 +13,55 @@ export const StationSelector = () => {
     setToggledSong(id);
   }
   return (
-    <div>
+    <div className="dark:text-gray-200 text-gray-800">
       <div className="flex justify-between">
-        <div className="form-check">
-          <input
-            className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-            type="radio"
-            name="flexRadioDefault"
-            id="5qap5aO4i9A"
-            onClick={setSongId}
-          />
-          <label className="form-check-label inline-block text-gray-800">
+        <div className="form-check flex-1">
+          <label className="form-check-label inline-block cursor-pointer">
+            <input
+              className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2"
+              type="radio"
+              name="flexRadioDefault"
+              id="5qap5aO4i9A"
+              onClick={setSongId}
+            />
             Default Lofi Station
           </label>
         </div>
-        <div className="form-check">
-          <input
-            className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-            type="radio"
-            name="flexRadioDefault"
-            id="0uw1Adx0psw"
-            onClick={setSongId}
-          />
-          <label className="form-check-label inline-block text-gray-800">
+        <div className="form-check flex-1">
+          <label className="form-check-label inline-block cursor-pointer">
+            <input
+              className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2"
+              type="radio"
+              name="flexRadioDefault"
+              id="0uw1Adx0psw"
+              onClick={setSongId}
+            />
             Lofi Hip Hop Station
           </label>
         </div>
       </div>
       <div className="flex justify-between">
-        <div className="form-check">
-          <input
-            className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-            type="radio"
-            name="flexRadioDefault"
-            id="aLqc8TdoLJ0"
-            onClick={setSongId}
-          />
-          <label className="form-check-label inline-block text-gray-800">
+        <div className="form-check flex-1">
+          <label className="form-check-label inline-block cursor-pointer">
+            <input
+              className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2"
+              type="radio"
+              name="flexRadioDefault"
+              id="aLqc8TdoLJ0"
+              onClick={setSongId}
+            />
             Rap Lofi Station
           </label>
         </div>
-        <div className="form-check">
-          <input
-            className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-            type="radio"
-            name="flexRadioDefault"
-            id="jWIqKujW0NY"
-            onClick={setSongId}
-          />
-          <label className="form-check-label inline-block text-gray-800">
+        <div className="form-check flex-1">
+          <label className="form-check-label inline-block cursor-pointer">
+            <input
+              className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2"
+              type="radio"
+              name="flexRadioDefault"
+              id="jWIqKujW0NY"
+              onClick={setSongId}
+            />
             Tokyo Lofi Station
           </label>
         </div>
