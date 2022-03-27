f03790d3af9b838340ad6d198b2717c12853aa7e
Alex Moks <53410236+Amokstakov@users.noreply.github.com>
2022-03-27 12:49:39 -0700

    adding dropdown menu for backgrounds (#23)
    * adding dropdown menu for backgrounds
    
    * Removing unused nav item
    
    * moving side bg nav
    
    * adding menuRef

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,35 +1,24 @@
 import {
-  useSetBackground,
   useToggleMusic,
   useToggleTimer,
   useToggleTasks,
   useSpotifyMusic,
 } from "../store";
-import { Button } from "../components/Common/Button";
 import { Player } from "../components/Player/Player";
 import { Timer } from "../components/Timer/Timer";
 import { TaskTracker } from "../components/TaskTracker/TaskTracker";
 import { Spotify } from "../components/Player/Spotify/Player";
+import { BackgroundNav } from "../components/Nav/BackgroundNav";
 
 export const HomePage = ({ backgrounds }: { backgrounds: any }) => {
-  const { setIsBackground } = useSetBackground();
   const { isMusicToggled } = useToggleMusic();
   const { isTimerToggled } = useToggleTimer();
   const { isTasksToggled } = useToggleTasks();
   const { isSpotifyToggled } = useSpotifyMusic();
+
   return (
     <div className="h-full w-full space-y-1">
-      <div className="flex justify-end">
-        <Button onClick={() => setIsBackground(backgrounds.SPACE)}>
-          Button
-        </Button>
-        <Button onClick={() => setIsBackground(backgrounds.STARS)}>
-          Button
-        </Button>
-        <Button onClick={() => setIsBackground(backgrounds.CITY)}>
-          Button
-        </Button>
-      </div>
+      <BackgroundNav backgrounds={backgrounds} />
       {isMusicToggled && (
         <div className="flex justify-center">
           <Player />
