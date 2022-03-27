0b636dca791f507d082ddbcb45b8f8552fef07d0
Amokstakov <amoksyak@uoguelph.ca>
2022-03-27 12:23:16 -0700

    moving side bg nav

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 import {
-  useSetBackground,
   useToggleMusic,
   useToggleTimer,
   useToggleTasks,
@@ -10,75 +8,17 @@ import { Player } from "../components/Player/Player";
 import { Timer } from "../components/Timer/Timer";
 import { TaskTracker } from "../components/TaskTracker/TaskTracker";
 import { Spotify } from "../components/Player/Spotify/Player";
+import { BackgroundNav } from "../components/Nav/BackgroundNav";
 
 export const HomePage = ({ backgrounds }: { backgrounds: any }) => {
-  const { setIsBackground } = useSetBackground();
   const { isMusicToggled } = useToggleMusic();
   const { isTimerToggled } = useToggleTimer();
   const { isTasksToggled } = useToggleTasks();
   const { isSpotifyToggled } = useSpotifyMusic();
-  const [showMenu, setShowMenu] = useState(false);
 
   return (
     <div className="h-full w-full space-y-1">
-      <div className="flex justify-end">
-        <div className="relative inline-block text-left">
-          <div>
-            <button
-              type="button"
-              className="inline-flex justify-center w-full rounded-l-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none"
-              onClick={() => setShowMenu(!showMenu)}
-            >
-              Choose Background
-              <svg
-                className="-mr-1 ml-2 h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
-
-          {showMenu && (
-            <div
-              className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none"
-              role="menu"
-            >
-              <div
-                className="py-1 cursor-pointer hover:bg-gray-100 hover:rounded-md"
-                onClick={() => setIsBackground(backgrounds.SPACE)}
-              >
-                <div className="text-gray-700 block px-4 py-2 text-sm">
-                  Space
-                </div>
-              </div>
-              <div
-                className="py-1 cursor-pointer hover:bg-gray-100 hover:rounded-md"
-                onClick={() => setIsBackground(backgrounds.STARS)}
-              >
-                <div className="text-gray-700 block px-4 py-2 text-sm">
-                  Stars
-                </div>
-              </div>
-              <div
-                className="py-1 cursor-pointer hover:bg-gray-100 hover:rounded-md"
-                onClick={() => setIsBackground(backgrounds.CITY)}
-              >
-                <div className="text-gray-700 block px-4 py-2 text-sm">
-                  City
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
+      <BackgroundNav backgrounds={backgrounds} />
       {isMusicToggled && (
         <div className="flex justify-center">
           <Player />
