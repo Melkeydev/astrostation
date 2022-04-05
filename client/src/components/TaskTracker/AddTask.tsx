477ff289917e81e91b296e689cbb9be43f24a71b
Amokstakov <amoksyak@uoguelph.ca>
2022-04-04 21:25:34 -0700

    Finishing add char counter

--- client/src/components/TaskTracker/AddTask.tsx
+++ client/src/components/TaskTracker/AddTask.tsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useTask } from "../../store";
 import { Button } from "../Common/Button";
 
 export const AddTask = () => {
+  const limit = 100;
+
   const [text, setText] = useState("");
   const { addTask } = useTask();
   const [pomoCounter, setPomoCounter] = useState(1);
+  const [charCount, setCharCount] = useState(text.slice(0, limit));
+
+  const setFormattedContent = useCallback(
+    (text) => {
+      setCharCount(text.slice(0, limit));
+    },
+    [limit, setCharCount]
+  );
 
   const onSubmit = (e: any) => {
     e.preventDefault();
@@ -18,6 +28,7 @@ export const AddTask = () => {
     addTask(text, pomoCounter);
 
     setText("");
+    setCharCount("");
     setPomoCounter(1);
   };
 
@@ -34,14 +45,19 @@ export const AddTask = () => {
       <div className="my-5">
         <label className="block">Task</label>
         <input
-          className="w-full h-10 m-1 py-2 px-3 text-lg border border-gray-300"
+          className="w-full h-10 m-1 py-2 px-3 text-lg border border-gray-300 dark:bg-gray-700 dark:border-gray-500"
           type="text"
           placeholder="Add Task"
           value={text}
-          onChange={(e) => setText(e.target.value)}
+          onChange={(e) => {
+            setText(e.target.value);
+            setFormattedContent(e.target.value);
+          }}
         />
+        <p className="m-1">
+          {charCount.length}/{limit}
+        </p>
       </div>
-      {/*Set Reminder should trigger after every finished session*/}
       <div className="my-5 flex items-center justify-center">
         <label className="flex-1">Set Pomodoro Counts</label>
         <div className="bg-gray-200 dark:bg-gray-700 dark:text-gray-200">
