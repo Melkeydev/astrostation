89965731c65c381b60ff835e32fe8c5649dca2e0
Alex Moks <53410236+Amokstakov@users.noreply.github.com>
2022-04-10 14:40:01 -0700

    Adding hacky fix (#50)

--- client/src/components/Player/Spotify/Player.tsx
+++ client/src/components/Player/Spotify/Player.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
+import { AiOutlineReload } from "react-icons/ai";
 import { useSpotifyMusic } from "../../../store";
 export const Spotify = () => {
   const { setIsSpotifyToggled } = useSpotifyMusic();
+  const [text, setText] = useState("");
+  const [playlist, setPlaylist] = useState(
+    "https://open.spotify.com/embed/playlist/37i9dQZF1DWWQRwui0ExPn"
+  );
+
+  function changePlaylist() {
+    if (!text.includes("https://open.spotify.com/playlist/")) {
+      alert("Invalid spotify URL");
+      return;
+    }
+    const splitOn = (slicable, ...indices) =>
+      [0, ...indices].map((n, i, m) => slicable.slice(n, m[i + 1]));
+    const stitchUrl = splitOn(text, 24)[0] + "/embed" + splitOn(text, 24)[1];
+    setPlaylist(stitchUrl);
+    setText("");
+  }
+
   return (
     <div className="py-2 mb-2 max-w-sm text-gray-800 shadow-md rounded-lg dark:text-gray-300 bg-white dark:bg-gray-800 dark:border-gray-700 w-1/2 justify-between">
       <div className="flex justify-between items-center p-1">
@@ -13,12 +32,27 @@ export const Spotify = () => {
       </div>
       <div className="justify-center">
         <iframe
-          src="https://open.spotify.com/embed/playlist/37i9dQZF1DWWQRwui0ExPn?utm_source=generator&theme=0"
+          src={`${playlist}?utm_source=generator&theme=0`}
           height="380"
           width="100%"
           frameBorder="0"
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         ></iframe>
+      </div>
+      <div className="flex items-center space-x-1 p-1">
+        <input
+          className="w-full p-1 border border-gray-300 dark:bg-gray-700 dark:border-gray-500"
+          type="text"
+          value={text}
+          placeholder="Ctrl-V Spotify URL here"
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+        <AiOutlineReload
+          className="w-5 cursor-pointer hover:text-slate-500"
+          onClick={changePlaylist}
+        />
       </div>
     </div>
   );
