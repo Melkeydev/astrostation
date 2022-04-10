ef49de369f22b27a2fa41623515da12c31ef9361
Amokstakov <amoksyak@uoguelph.ca>
2022-04-10 11:33:59 -0700

    finishing crypto

--- client/src/components/Crypto/Modal.tsx
+++ client/src/components/Crypto/Modal.tsx
@@ -5,6 +5,7 @@ import { Button } from "../Common/Button";
 import ErrorMessage from "./Error";
 import TxList from "./HashList";
 import { IoCloseSharp } from "react-icons/io5";
+import { FaEthereum } from "react-icons/fa";
 
 const startPayment = async ({ setError, setTxs, ether, addr }) => {
   try {
@@ -28,9 +29,17 @@ const startPayment = async ({ setError, setTxs, ether, addr }) => {
   }
 };
 
+const isMetaMaskConnected = async () => {
+  //@ts-ignore
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const accounts = await provider.listAccounts();
+  return accounts.length > 0;
+};
+
 export const Modal = ({ isVisible = false, onClose }) => {
   const [error, setError] = useState("");
   const [txs, setTxs] = useState([]);
+  const [metaMaskConnected, setMetaMaskConnected] = useState(false);
   const keydownHandler = ({ key }) => {
     switch (key) {
       case "Escape":
@@ -40,11 +49,37 @@ export const Modal = ({ isVisible = false, onClose }) => {
     }
   };
 
+  const connectMetaMaskConnected = async () => {
+    try {
+      //@ts-ignore
+      if (!window.ethereum)
+        throw new Error("No crypto wallet found. Please install it.");
+
+      //@ts-ignore
+      await window.ethereum.send("eth_requestAccounts");
+      //@ts-ignore
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      provider.getSigner();
+      setTxs([{ hash: "MetaMask wallet connected" }]);
+      setMetaMaskConnected(true);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const address = "0x39CFE0572DE24a92df064C8Fdf4C23DE6a2d6628";
     const data = new FormData(e.target);
+
     setError("");
+    setTxs([]);
+
+    if (!data.get("ether")) {
+      setError("Please input value to donate");
+      return;
+    }
+
     await startPayment({
       setError,
       setTxs,
@@ -52,6 +87,16 @@ export const Modal = ({ isVisible = false, onClose }) => {
       addr: address,
     });
   };
+
+  useEffect(() => {
+    isMetaMaskConnected().then((connected) => {
+      if (connected) {
+        setMetaMaskConnected(true);
+      } else {
+        setMetaMaskConnected(false);
+      }
+    });
+  }, [isVisible]);
 
   useEffect(() => {
     document.addEventListener("keydown", keydownHandler);
@@ -72,30 +117,45 @@ export const Modal = ({ isVisible = false, onClose }) => {
         <div className="">
           <div className="w-full">
             <form className="" onSubmit={handleSubmit}>
-              <div className="credit-card w-full sm:w-auto shadow-lg mx-auto rounded-xl bg-white">
-                <main className="p-4">
+              <div className="w-full sm:w-auto shadow-lg mx-auto rounded-xl bg-white">
+                <main className="pl-4 pr-4 pb-4">
                   <h1 className="text-xl font-semibold text-gray-700 text-center">
                     Connect MetaMask Wallet and Donate
                   </h1>
                   <div>
-                    <div className="my-3">
+                    <div className="flex items-center h-10">
+                      <div className="flex items-center px-4 py-2 bg-violet-700 text-white font-medium focus:outline-none dark:bg-gray-800 dark:text-gray-200">
+                        <FaEthereum className="h-6 w-6" />
+                        ETH
+                      </div>
                       <input
                         name="ether"
                         type="text"
                         className="w-full h-10 m-1 py-2 px-3 text-lg border border-gray-300 dark:bg-gray-700 dark:border-gray-500 block focus:ring focus:outline-none"
-                        placeholder="Amount in ETH"
+                        placeholder="0.00197 ETH"
                       />
                     </div>
                   </div>
                 </main>
                 <footer className="p-4">
-                  <Button
-                    variant="crypto"
-                    type="submit"
-                    className="focus:ring focus:outline-none w-full"
-                  >
-                    Donate Now
-                  </Button>
+                  {!metaMaskConnected ? (
+                    <Button
+                      variant="crypto"
+                      type="button"
+                      className="focus:ring focus:outline-none w-full"
+                      onClick={() => connectMetaMaskConnected()}
+                    >
+                      Connect MetaMask wallet
+                    </Button>
+                  ) : (
+                    <Button
+                      variant="crypto"
+                      type="submit"
+                      className="focus:ring focus:outline-none w-full"
+                    >
+                      Donate Now
+                    </Button>
+                  )}
                   <ErrorMessage message={error} />
                   <TxList txs={txs} />
                 </footer>
