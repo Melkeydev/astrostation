39fad438d12e214d2fcf13281b99c723291337af
Amokstakov <amoksyak@uoguelph.ca>
2022-04-14 22:06:44 -0700

    saving

--- client/src/components/Dragggable/Draggable.tsx
+++ client/src/components/Dragggable/Draggable.tsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import Draggable from "react-draggable";
 import "./Draggable.scss";
-export const DWrapper = ({ children, toggleHook }) => {
+let int = 0;
+export const DWrapper = ({
+  children,
+  toggleHook,
+  defaultX,
+  defaultY,
+  dragRef,
+}) => {
+  const [z, setZ] = useState(0);
+
+  function trackPosition(data) {
+    setZ(++int);
+  }
+
   return (
-    <Draggable bounds="parent">
-      <div className="box dwidth">
+    <Draggable
+      bounds="parent"
+      defaultPosition={{ x: defaultX, y: defaultY }}
+      onDrag={(e, data) => trackPosition(data)}
+    >
+      <div
+        style={{ zIndex: z, position: "relative" }}
+        className="dcard box dwidth"
+        onClick={() => setZ(++int)}
+      >
         <div
+          ref={dragRef}
           className={`${
             toggleHook ? "visible" : "invisible pointer-events-none"
           }`}
--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, useRef } from "react";
 import {
   useToggleMusic,
   useToggleTimer,
@@ -12,11 +13,7 @@ import { Spotify } from "../components/Player/Spotify/Player";
 import { BackgroundNav } from "../components/Nav/BackgroundNav";
 import { TimerSettings } from "../components/Timer/Settings";
 import { GoGear } from "react-icons/go";
-
 import { Donations } from "../components/Crypto/Donations";
-
-import Draggable from "react-draggable";
-
 import { DWrapper } from "../components/Dragggable/Draggable";
 
 export const HomePage = ({ backgrounds }: { backgrounds: any }) => {
@@ -25,6 +22,58 @@ export const HomePage = ({ backgrounds }: { backgrounds: any }) => {
   const { isTasksToggled } = useToggleTasks();
   const { isSpotifyToggled } = useSpotifyMusic();
   const { isSettingsToggled, setIsSettingsToggled } = useToggleSettings();
+  const [isMobile, setIsMobile] = useState(false);
+
+  const [testX, setTestX] = useState(window.innerWidth * 0.8);
+
+  console.log(testX);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    const listener = () => {
+      triggerMouseEvent(ref.current, "mouseover");
+      triggerMouseEvent(ref.current, "mousedown");
+      triggerMouseEvent(document, "mousemove");
+      triggerMouseEvent(ref.current, "mouseup");
+      triggerMouseEvent(ref.current, "click");
+    };
+
+    addEventListener("resize", listener);
+    return () => removeEventListener("resize", listener);
+  }, []);
+
+  const triggerMouseEvent = (element, eventType) => {
+    const mouseEvent = document.createEvent("MouseEvents");
+
+    mouseEvent.initEvent(eventType, true, true);
+    element.dispatchEvent(mouseEvent);
+  };
+
+  const [screenSize, getDimension] = useState({
+    dynamicWidth: window.innerWidth,
+  });
+
+  const setDimension = () => {
+    if (window.innerWidth < 641) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+    getDimension({
+      dynamicWidth: window.innerWidth,
+    });
+
+    setTestX(window.innerWidth * 0.5);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", setDimension);
+
+    return () => {
+      window.removeEventListener("resize", setDimension);
+    };
+  }, [screenSize, testX]);
 
   return (
     <div className="h-screen w-70 space-y-1">
@@ -39,21 +88,68 @@ export const HomePage = ({ backgrounds }: { backgrounds: any }) => {
         </button>
         <BackgroundNav backgrounds={backgrounds} />
       </div>
-      <DWrapper toggleHook={isMusicToggled}>
-        <Player />
-      </DWrapper>
-      <DWrapper toggleHook={isSpotifyToggled}>
-        <Spotify />
-      </DWrapper>
-      <DWrapper toggleHook={isSettingsToggled}>
-        <TimerSettings />
-      </DWrapper>
-      <DWrapper toggleHook={isTimerToggled}>
-        <Timer />
-      </DWrapper>
-      <DWrapper toggleHook={isTasksToggled}>
-        <TaskTracker />
-      </DWrapper>
+      {isMobile ? (
+        <div className="flex flex-col items-center ml-8">
+          <div className={`${isMusicToggled ? "block" : "hidden"}`}>
+            <Player />
+          </div>
+          <div className={`${isSpotifyToggled ? "block" : "hidden"}`}>
+            <Spotify />
+          </div>
+          <div className={`${isSettingsToggled ? "block" : "hidden"}`}>
+            <TimerSettings />
+          </div>
+          <div className={`${isTimerToggled ? "block" : "hidden"}`}>
+            <Timer />
+          </div>
+          <div className={`${isTasksToggled ? "block" : "hidden"}`}>
+            <TaskTracker />
+          </div>
+        </div>
+      ) : (
+        <>
+          <DWrapper
+            toggleHook={isMusicToggled}
+            defaultX={750}
+            defaultY={0}
+            dragRef={ref}
+          >
+            <Player />
+          </DWrapper>
+          <DWrapper
+            toggleHook={isSpotifyToggled}
+            defaultX={300}
+            defaultY={100}
+            dragRef={ref}
+          >
+            <Spotify />
+          </DWrapper>
+          <DWrapper
+            toggleHook={isSettingsToggled}
+            defaultX={750}
+            defaultY={-200}
+            dragRef={ref}
+          >
+            <TimerSettings />
+          </DWrapper>
+          <DWrapper
+            toggleHook={isTimerToggled}
+            defaultX={750}
+            defaultY={-745}
+            dragRef={ref}
+          >
+            <Timer />
+          </DWrapper>
+          <DWrapper
+            toggleHook={isTasksToggled}
+            defaultX={300}
+            defaultY={-1215}
+            dragRef={ref}
+          >
+            <TaskTracker />
+          </DWrapper>
+        </>
+      )}
       <div className="fixed bottom-0">
         <Donations />
       </div>
