67395d28dd91a9bb6726c4f5cbeb74e2134c6f48
Arnold Chand <creativenull@outlook.com>
2022-04-18 13:58:31 -0400

    feat: add path aliases

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import * as path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@App": path.resolve(__dirname, "./src"),
+      "@Components": path.resolve(__dirname, "src/components"),
+      "@Store": path.resolve(__dirname, "src/store"),
+      "@Utils": path.resolve(__dirname, "src/utils"),
+      "@Pages": path.resolve(__dirname, "src/pages"),
+    },
+  },
+});
