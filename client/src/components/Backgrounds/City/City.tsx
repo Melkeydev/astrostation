5ff64a91b11761e1b3ab4da5ac166ba1705f11ea
Alex Moks <53410236+Amokstakov@users.noreply.github.com>
2022-04-18 08:28:43 -0700

    Issue batches apr16 (#62)
    * Adding favicon
    
    * Fixing issue#45
    
    * fixing #54
    
    * fixing long break

--- client/src/components/Backgrounds/City/City.tsx
+++ client/src/components/Backgrounds/City/City.tsx
@@ -1,15 +1,9 @@
 import "./City.scss";
-import { CircleContainer } from "./CircleContainer";
 
 export const City = () => {
   return (
     <div className="cityContainer">
-      <img
-        className="cityBackground"
-        src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/221808/sky.jpg"
-      />
-
-      {Array(99).fill(<CircleContainer />)}
+      {Array(150).fill(<div className="falling"></div>)}
     </div>
   );
 };
