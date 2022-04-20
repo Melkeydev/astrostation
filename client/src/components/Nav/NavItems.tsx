3ca41d2fd5afa294f7d72276754d4db17e3b45ac
Amokstakov <amoksyak@uoguelph.ca>
2022-04-20 13:34:40 -0700

    fixing nav hover css

--- client/src/components/Nav/NavItems.tsx
+++ client/src/components/Nav/NavItems.tsx
@@ -10,7 +10,7 @@ export const NavItem: FC<{ onClick?: () => void; toggled?: boolean }> = ({
       <button
         className={`${
           toggled &&
-          "bg-gray-200 text-black border-black border-b-2 dark:bg-violet-500"
+          "bg-gray-200 text-gray-800 dark:text-black border-black border-b-2 dark:bg-violet-500"
         } sm:h-16 sm:px-6  flex justify-center items-center w-full text-gray-300 hover:bg-gray-200 hover:text-gray-800 h-14 px-4 dark:hover:bg-violet-500`}
         onClick={onClick}
       >
