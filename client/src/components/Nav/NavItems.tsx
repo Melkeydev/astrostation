89d831e1d0e9ccf08426c113fb2a8c0bae0485be
Alex Moks <53410236+Amokstakov@users.noreply.github.com>
2022-05-15 14:28:20 -0700

    Quick fix for toggle issue on mobile (#113)
    * fixing
    
    * removing added viewport

--- client/src/components/Nav/NavItems.tsx
+++ client/src/components/Nav/NavItems.tsx
@@ -11,7 +11,7 @@ export const NavItem: FC<{ onClick?: () => void; toggled?: boolean }> = ({
         className={`${
           toggled &&
           "bg-gray-200 text-black dark:text-black border-black border-b-2 dark:bg-violet-500"
-        } sm:h-16 sm:px-6  flex justify-center items-center w-full text-gray-300 hover:bg-gray-200 hover:text-gray-800 h-14 px-4 dark:hover:bg-violet-500`}
+        } sm:h-16 sm:px-6  flex justify-center items-center w-full text-gray-300 md:hover:bg-gray-200 md:hover:text-gray-800 h-14 px-4 md:dark:hover:bg-violet-500`}
         onClick={onClick}
       >
         {children}
