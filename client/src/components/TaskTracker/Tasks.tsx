9ed9f0d6b680302319e2531517b08bc6bd4dff3d
Amokstakov <amoksyak@uoguelph.ca>
2022-05-22 12:34:20 -0700

    Adding clear all feature

--- client/src/components/TaskTracker/Tasks.tsx
+++ client/src/components/TaskTracker/Tasks.tsx
@@ -1,11 +1,32 @@
 import { Task } from "./Task";
+import { Button } from "@Components/Common/Button";
+import { useTask } from "@Store";
 
 export const Tasks = ({ tasks }: any) => {
+  // TODO: Add Clear all at the bottom of this component
+  // TODO: add the hook funcitonality to delete all the tasks in the list
+  //
+  const { removeAllTasks } = useTask();
+
+  function confirmClearTasks() {
+    var answer = window.confirm("This will clear all current tasks");
+    if (answer) {
+      removeAllTasks();
+    }
+  }
+
   return (
     <>
       {tasks.map((task, index) => (
         <Task key={index} task={task} />
       ))}
+      {tasks && (
+        <div className="flex justify-end">
+          <Button variant="danger" onClick={() => confirmClearTasks()}>
+            Clear All
+          </Button>
+        </div>
+      )}
     </>
   );
 };
