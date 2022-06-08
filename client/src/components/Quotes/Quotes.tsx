4cb15abfa7585cc18ec1652e22447b6498e8c175
Alex Moks <53410236+Amokstakov@users.noreply.github.com>
2022-06-07 19:13:01 -0700

    Bug batch june 6 (#166)
    * issue-92
    
    * fix issue-118
    
    * issue-161
    
    * issue160
    
    * issue-163
    
    * Adding zustand state

--- client/src/components/Quotes/Quotes.tsx
+++ client/src/components/Quotes/Quotes.tsx
@@ -14,16 +14,21 @@ export const Quotes = () => {
   }, []);
 
   return (
-    <div className="shadow-lg mb-2 max-w-sm w-72 sm:w-96 bg-white/[.96] rounded-lg border border-gray-200 dark:text-gray-300 dark:bg-gray-800/[.96] dark:border-gray-700 px-8 py-4 text-center">
-      <div className="text-gray-800 dark:text-white relative items-center justify-center text-xl font-radio-canada mr-4">
-        {quoteData[quoteNumber].q}
-        <br />-{quoteData[quoteNumber].a}
+    <div className="sm:w-96 bg-white/[.96] rounded-lg border border-gray-200 dark:text-gray-300 dark:bg-gray-800/[.96] dark:border-gray-700">
+      <div className="flex justify-end w-full p-2 handle cursor-move">
         <IoCloseSharp
-          className="text-red-500 cursor-pointer hover:bg-red-200 absolute top-[0] right-[-30px]"
+          className="text-red-500 cursor-pointer hover:bg-red-200"
           onClick={() => setIsQuoteToggled(false)}
         />
+      </div>
+      <div className="max-w-sm text-center">
+        <div className="text-gray-800 dark:text-white relative items-center justify-center text-xl font-radio-canada pb-2 pr-2 pl-2">
+          {quoteData[quoteNumber].q}
+          <br />-{quoteData[quoteNumber].a}
+        </div>
+      </div>
+      <div className="flex justify-end pb-2 pr-2 pl-2 w-full text-base">
         <AiOutlineReload
-          className="text-gray-800 dark:text-white cursor-pointer absolute top-[80%] right-[-30px]"
           onClick={() =>
             setQuoteNumber(Math.floor(Math.random() * quoteData.length))
           }
