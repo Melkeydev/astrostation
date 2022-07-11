5a3d4aa8a6e83c1d3b57a42536c05230fbfc8f8f
Matt Privman <1153991+mattprivman@users.noreply.github.com>
2022-07-11 11:46:15 -0600

    Fix background-size: cover for video

--- client/src/components/Backgrounds/LofiGirl/LofiGirl.tsx
+++ client/src/components/Backgrounds/LofiGirl/LofiGirl.tsx
@@ -2,7 +2,7 @@ import "./LofiGirl.scss";
 
 export const LofiGirl = () => {
   return (
-    <div className="backgroundContainer">
+    <div className="backgroundContainer-LofiGirl">
       <video autoPlay muted loop id="myVideo">
         <source src="/assets/videos/lofi_girl.mp4" type="video/mp4" />
       </video>
