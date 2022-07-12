b1f4e7ac616d459797a9c059741ec2f91f56f041
Matt Privman <1153991+mattprivman@users.noreply.github.com>
2022-07-11 18:19:52 -0600

    Fix sticky note count going to lowest nav item

--- client/src/components/Nav/NavItems.tsx
+++ client/src/components/Nav/NavItems.tsx
@@ -12,7 +12,7 @@ export const NavItem: FC<{ onClick?: () => void; toggled?: boolean; shown? : boo
         <button
           className={`${
             toggled &&
-            "bg-gray-200 text-black dark:text-black border-black border-b-2 dark:bg-violet-500"
+            "relative bg-gray-200 text-black dark:text-black border-black border-b-2 dark:bg-violet-500"
           } sm:h-16 sm:px-6  flex justify-center items-center w-full text-gray-300 md:hover:bg-gray-200 md:hover:text-gray-800 h-14 px-4 md:dark:hover:bg-violet-500`}
           onClick={onClick}
         >
