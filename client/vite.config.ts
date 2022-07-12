fb404fefd14cc077cdcb8307c3e18f0dadd68701
Amokstakov <amoksyak@uoguelph.ca>
2022-07-11 21:30:46 -0700

    testing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/astrostation/",
+  base: "./",
   resolve: {
     alias: {
       "@Root": path.resolve(__dirname, "./"),
