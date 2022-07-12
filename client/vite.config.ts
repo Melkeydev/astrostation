3287272f657f8cd6f8167371f056da2ad3f67cd3
Amokstakov <amoksyak@uoguelph.ca>
2022-07-11 21:53:55 -0700

    Fixing route

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/astrostation/",
   resolve: {
     alias: {
       "@Root": path.resolve(__dirname, "./"),
