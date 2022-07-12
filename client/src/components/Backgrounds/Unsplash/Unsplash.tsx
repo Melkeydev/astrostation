a38f5bc89abaf7ed661ae6d23bf7f893c41d6963
Amokstakov <amoksyak@uoguelph.ca>
2022-07-11 20:05:36 -0700

    Adding more staging changes

--- client/src/components/Backgrounds/Unsplash/Unsplash.tsx
+++ client/src/components/Backgrounds/Unsplash/Unsplash.tsx
@@ -2,12 +2,18 @@ import { useUnsplashStore } from "@Root/src/store";
 import "./Unsplash.scss";
 
 export const Unsplash = () => {
-  const { dailyUnsplash } = useUnsplashStore()
+  const { dailyUnsplash } = useUnsplashStore();
 
   return (
     <div>
-      <div className="unsplash-container" style={{ background: `url(${ dailyUnsplash.urls ? dailyUnsplash.urls.raw + "&dpr=2&w=1699" : ""}) no-repeat center center fixed`}}>
-      </div>
+      <div
+        className="unsplash-container"
+        style={{
+          background: `url(${
+            dailyUnsplash.urls ? dailyUnsplash.urls.raw + "&dpr=2&w=1699" : ""
+          }) no-repeat center center fixed`,
+        }}
+      ></div>
     </div>
   );
 };
