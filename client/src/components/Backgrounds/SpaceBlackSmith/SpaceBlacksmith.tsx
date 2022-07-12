23a946910bfe71922e1fd37419691f06f766e506
Matt Privman <1153991+mattprivman@users.noreply.github.com>
2022-07-11 23:23:47 -0600

    Fix file paths Train & Blacksmith

--- client/src/components/Backgrounds/SpaceBlackSmith/SpaceBlacksmith.tsx
+++ client/src/components/Backgrounds/SpaceBlackSmith/SpaceBlacksmith.tsx
@@ -1,10 +1,11 @@
 import "../Backgrounds.scss";
+import spaceBlackSmith from "/assets/videos/space_blacksmith.mp4";
 
 export const SpaceBlacksmith = () => {
   return (
     <div className="background-container">
       <video autoPlay muted loop id="spaceBlacksmithVideo">
-        <source src="/assets/videos/space_blacksmith.mp4" type="video/mp4" />
+        <source src={spaceBlackSmith} type="video/mp4" />
       </video>
     </div>
   );
