1365fc29e34156e0b2274fb6eb00a9c8f642c5b6
Amokstakov <amoksyak@uoguelph.ca>
2022-09-04 10:45:06 -0700

    Finish task tracker info update

--- client/src/components/TaskTracker/TaskTracker.tsx
+++ client/src/components/TaskTracker/TaskTracker.tsx
@@ -2,17 +2,27 @@ import { useState } from "react";
 import { Header } from "./Header";
 import { Tasks } from "./Tasks";
 import { AddTask } from "./AddTask";
-import { IoCloseSharp } from "react-icons/io5";
+import { IoCloseSharp, IoInformationCircleOutline } from "react-icons/io5";
 import { useTask, useToggleTasks } from "@Store";
+import { TaskInfoModal } from "@App/components/TaskTracker/InfoModal";
 
 export const TaskTracker = () => {
   const [showAddTask, setShowAddTask] = useState(false);
   const { setIsTasksToggled } = useToggleTasks();
   const { tasks } = useTask();
+  const [isTaskInfoModalOpen, setIsTaskInfoModalOpen] = useState(false);
 
   return (
     <div className="bg-white/[.96] rounded-lg border border-gray-200 shadow-md dark:bg-gray-800/[.96] dark:border-gray-700">
-      <div className="flex justify-end handle p-2 w-full cursor-move">
+      <div className="flex justify-between handle p-2 w-full cursor-move">
+        <TaskInfoModal
+          isVisible={isTaskInfoModalOpen}
+          onClose={() => setIsTaskInfoModalOpen(false)}
+        />
+        <IoInformationCircleOutline
+          className="cursor-pointer text-white"
+          onClick={() => setIsTaskInfoModalOpen(true)}
+        />
         <IoCloseSharp
           className="text-red-500 cursor-pointer hover:bg-red-200"
           onClick={() => setIsTasksToggled(false)}
