8f6f9e5e19fb547224a529ced9608d0fe592205a
Ahmed Haracic <me@qoobes.space>
2022-10-25 02:42:23 +0200

    Remove MUI and rebuild popover

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,14 @@
+const animations = require("./src/theme/animations.js");
+
 module.exports = {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      ...animations,
       fontFamily: {
-        'radio-canada': ['"Radio Canada"', 'sans-serif']
-      }
+        "radio-canada": ['"Radio Canada"', "sans-serif"],
+      },
     },
   },
   plugins: [],
