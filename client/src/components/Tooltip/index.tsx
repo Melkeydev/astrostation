91fad0b32d8948ca618d16d218f9625121caae85
Ahmed Haracic <me@qoobes.space>
2022-10-25 02:42:23 +0200

    Remove MUI and rebuild popover

---
+++ client/src/components/Tooltip/index.tsx
@@ -0,0 +1,49 @@
+import { flip, offset, Placement } from "@floating-ui/core";
+import { shift, useFloating } from "@floating-ui/react-dom";
+import clsx from "clsx";
+import React, { useState } from "react";
+
+type TooltipProps = {
+  text: string;
+  children: React.ReactNode;
+  position?: Placement;
+};
+
+export const WithTooltip = ({
+  text,
+  children,
+  position = "top",
+}: TooltipProps) => {
+  const [isOpen, setIsOpen] = useState<boolean | undefined>(undefined);
+  const { x, y, reference, floating, strategy } = useFloating({
+    placement: position,
+    middleware: [shift(), flip(), offset(10)],
+  });
+
+  return (
+    <>
+      {React.cloneElement(children as React.ReactElement, {
+        onMouseEnter: () => setIsOpen(true),
+        onMouseLeave: () => setIsOpen(false),
+        ref: reference,
+      })}
+
+      <div
+        ref={floating}
+        className={clsx(
+          "py-0.5 px-2 rounded-md bg-slate-100 dark:bg-gray-700 text-black dark:text-white text-sm",
+          isOpen && "animate-intro-fade-scale",
+          isOpen === false && "animate-outro-fade-scale",
+          isOpen === undefined && "opacity-0"
+        )}
+        style={{
+          position: strategy,
+          top: y ?? "",
+          left: x ?? "",
+        }}
+      >
+        {text}
+      </div>
+    </>
+  );
+};
