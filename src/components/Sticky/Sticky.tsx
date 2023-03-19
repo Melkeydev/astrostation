a80d194ac89eca94d3d7156b1e90216d7006438a
Paul Nguon <herropaul@gmail.com>
2023-03-18 17:40:25 -0700

    added color selector to sticky note componenets

--- src/components/Sticky/Sticky.tsx
+++ src/components/Sticky/Sticky.tsx
@@ -1,13 +1,52 @@
-import { IoCloseSharp } from "react-icons/io5";
+import { IoCloseSharp, IoEllipsisHorizontalSharp } from "react-icons/io5";
+import { useState } from "react";
 import { useStickyNote } from "@Store";
 import TextareaAutosize from "react-textarea-autosize";
 
 export const Sticky = ({ id, text }) => {
   const { removeNote, editNote } = useStickyNote();
+  const [showColorSelector, setShowColorSelector] = useState(false);
+  const [color, setColor] = useState("#feff9c");
+
+  // Toggles the state of the color selector open/closed
+  const toggleColorSelector = () => {
+    setShowColorSelector((prevState) => !prevState);
+  };
+
+  // Sets the selected color and closes the color selector
+  const selectColor = (selectedColor) => {
+    setColor(selectedColor);
+    toggleColorSelector();
+  };
+
+  // Renders a row of color elements
+  const displayColors = () => {
+    const colors = ["#feff9c", "#d1fae5", "#f6ccd7", "#e0bbff", "#a7cdfa"];
+    return (
+      <div className="mb-1 flex">
+        {colors.map((c) => (
+          <div
+            key={c}
+            className="h-10 w-10 cursor-pointer"
+            style={{ backgroundColor: c }}
+            onClick={() => selectColor(c)}
+          />
+        ))}
+      </div>
+    );
+  };
 
   return (
-    <div className="cursor-move bg-[#feff9c]">
+    <div
+      className="cursor-move bg-[#feff9c]"
+      style={{ backgroundColor: color }}
+    >
+      {showColorSelector && displayColors()}
       <div className="flex w-full justify-end p-2">
+        <IoEllipsisHorizontalSharp
+          className="mr-2 cursor-pointer"
+          onClick={toggleColorSelector}
+        />
         <IoCloseSharp
           className="cursor-pointer text-red-500 hover:bg-red-200"
           onClick={() => removeNote(id)}
