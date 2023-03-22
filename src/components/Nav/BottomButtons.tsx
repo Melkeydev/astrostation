0acf1aac117f2b9b6758ff23cfb87686659202c7
Madeleine Belanger <madeleinepbelanger@gmail.com>
2023-03-22 15:44:36 -0700

    added bottom button

---
+++ src/components/Nav/BottomButtons.tsx
@@ -0,0 +1,18 @@
+import {Button} from "../Common/Button"
+import { AiOutlineGithub } from "react-icons/ai";
+import { WithTooltip } from "../Tooltip"
+
+function navigateToGithub(){
+window.open("https://github.com/MelkeyOSS/astrostation")
+}
+function BottomButtons(){
+return(
+    <WithTooltip text="Astrostation is an open source project and is open to contributions."> 
+        <div className="fixed bottom-2 ml-32">  
+            <Button  onClick={()=>navigateToGithub()} className="donateButton flex items-center rounded-md h-10 bg-violet-700  font-medium text-white shadow-sm focus:outline-none dark:bg-violet-700 dark:text-violet-200" >
+            <AiOutlineGithub className="h-[200%]"/>  
+           </Button>
+        </div>
+    </WithTooltip>
+)}
+export default BottomButtons      
