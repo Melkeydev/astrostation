ce33bd6dc53a63e1b84943f882f21bd2a6ec33af
EmalineG <mgayhart0@ivc.edu>
2023-03-21 21:18:04 -0700

    toggle opacity info button

--- src/components/SeoToggle/SeoToggle.tsx
+++ src/components/SeoToggle/SeoToggle.tsx
@@ -1,12 +1,20 @@
 import { FaInfo } from "react-icons/fa";
+import React, { useState } from 'react';
 
 export const SeoToggle = ({ onClick }: {onClick}) => {
+  const [isBgTransparent, setIsBgTransparent] = useState(false);
+
+  const handleClick = () => {
+    onClick(); 
+    setIsBgTransparent(!isBgTransparent); 
+  };
+
   return(
 <div className="fixed bottom-0 z-10 left-60">
         <button
-          onClick={onClick}
+          onClick={handleClick}
           type="button"
-          className="seoToggle flex items-center rounded-md bg-violet-700 ml-2 mb-2 px-4 py-2 font-medium text-white shadow-sm focus:outline-none dark:bg-violet-700 dark:text-violet-200"
+          className={`seoToggle flex items-center rounded-md bg-violet-700 ml-2 mb-2 px-4 py-2 font-medium text-white shadow-sm focus:outline-none ${ isBgTransparent ?  `dark:bg-violet-700` : `dark:bg-violet-700/[0.6]`} dark:text-violet-200`}
         >
           <FaInfo/>
         </button>
