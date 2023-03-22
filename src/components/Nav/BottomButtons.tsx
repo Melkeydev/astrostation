bede7ff2089519d73b59b23d9e0e29475deb9d4f
Madeleine Belanger <madeleinepbelanger@gmail.com>
2023-03-22 15:39:43 -0700

    Removed wording and updated contributers list

--- src/components/Nav/BottomButtons.tsx
+++ src/components/Nav/BottomButtons.tsx
@@ -9,9 +9,8 @@ function BottomButtons(){
 return(
     <WithTooltip text="Astrostation is an open source project and is open to contributions."> 
         <div className="fixed bottom-2 ml-32">  
-            <Button  onClick={()=>navigateToGithub()} className="donateButton flex items-center rounded-md bg-violet-700  font-medium text-white shadow-sm focus:outline-none dark:bg-violet-700 dark:text-violet-200" >
-            Github Repo         
-            <AiOutlineGithub className="ml-1"/>  
+            <Button  onClick={()=>navigateToGithub()} className="donateButton flex items-center rounded-md h-10 bg-violet-700  font-medium text-white shadow-sm focus:outline-none dark:bg-violet-700 dark:text-violet-200" >
+            <AiOutlineGithub className=""/>  
            </Button>
         </div>
     </WithTooltip>
