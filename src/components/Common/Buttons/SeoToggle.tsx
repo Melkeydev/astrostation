a256a51e9ae181a89e57d2a00547127f722c7bb2
EmalineG <mgayhart0@ivc.edu>
2023-03-22 17:25:04 -0700

    Changed <seotoggle> CTA to be more explicit

--- src/components/Common/Buttons/SeoToggle.tsx
+++ src/components/Common/Buttons/SeoToggle.tsx
@@ -16,7 +16,7 @@ export const SeoToggle = ({ onClick }: { onClick }) => {
           type="button"
           className={"seoToggle flex items-center rounded-md bg-violet-700 ml-2 mb-2 px-4 py-2 font-medium text-white shadow-sm focus:outline-none dark:bg-violet-900/[0.6]  dark:text-violet-200"}
         >
-         Back to Astrostation
+         Hide Content
         </button>
       </div>
   )
