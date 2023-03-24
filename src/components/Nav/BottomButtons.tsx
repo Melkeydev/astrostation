deab0644ff841121c121022c812a6981a7fd453f
Madeleine Belanger <madeleinepbelanger@gmail.com>
2023-03-23 23:11:04 -0700

    Changed size of GH logo

--- src/components/Nav/BottomButtons.tsx
+++ src/components/Nav/BottomButtons.tsx
@@ -1,6 +1,7 @@
 import {Button} from "../Common/Button"
 import { AiOutlineGithub } from "react-icons/ai";
 import { WithTooltip } from "../Tooltip"
+import { SiGithub } from "react-icons/si";
 
 function navigateToGithub(){
 window.open("https://github.com/MelkeyOSS/astrostation")
@@ -9,8 +10,8 @@ function BottomButtons(){
 return(
     <WithTooltip text="Astrostation is an open source project and is open to contributions."> 
         <div className="fixed bottom-2 ml-32">  
-            <Button  onClick={()=>navigateToGithub()} className="donateButton flex items-center rounded-md h-10 font-medium" variant="bottomButton">
-            <AiOutlineGithub className="#"/>  
+            <Button  onClick={()=>navigateToGithub()} className=" flex items-center rounded-md h-10 mw-10 w-14  " variant="bottomButton">
+            <SiGithub size={30} />  
            </Button>
         </div>
     </WithTooltip>
