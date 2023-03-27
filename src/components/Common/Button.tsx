55c1fcaf0731679ab5b15159b7861ef70851c4bf
Madeleine Belanger <madeleinepbelanger@gmail.com>
2023-03-26 20:07:35 -0700

    Changed navigate function to anchor tag + re-added space to crypto btn.

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -19,7 +19,7 @@ const classes: { [key: string]: any } = {
       "bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",
     cold: "bg-blue-grey-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",
     bottomButton:
-      "bg-violet-700 hover:bg-violet-800 text-white font-bold py-2 px-4 rounded shadow-sm  focus:outline-none dark:bg-violet-700 dark:text-violet-200",
+      "bg-violet-700 hover:bg-violet-800 text-white font-bold rounded shadow-sm  focus:outline-none dark:bg-violet-700 dark:text-violet-200",
     coldPrimary: "bg-blue-700 text-white font-bold py-2 px-4 rounded ",
   },
 };
--- src/components/Nav/BottomButtons.tsx
+++ src/components/Nav/BottomButtons.tsx
@@ -3,16 +3,16 @@ import { AiOutlineGithub } from "react-icons/ai";
 import { WithTooltip } from "../Tooltip"
 import { SiGithub } from "react-icons/si";
 
-function navigateToGithub(){
-window.open("https://github.com/MelkeyOSS/astrostation")
-}
 function BottomButtons(){
 return(
     <WithTooltip text="Astrostation is an open source project and is open to contributions."> 
         <div className="fixed bottom-2 ml-32">  
-            <Button  onClick={()=>navigateToGithub()} className=" flex items-center rounded-md h-10 mw-10 w-14  " variant="bottomButton">
-            <SiGithub size={30} />  
+            <a href="https://github.com/MelkeyOSS/astrostation"
+                target="_blank">
+                <Button  className=" flex items-center rounded-md h-10 w-14" variant="bottomButton">
+                <SiGithub size={23} />  
            </Button>
+           </a>
         </div>
     </WithTooltip>
 )}
