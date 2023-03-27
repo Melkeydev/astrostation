d04a2f28a361f0fcc350db548886af7980936a77
Madeleine Belanger <madeleinepbelanger@gmail.com>
2023-03-26 20:07:35 -0700

    Changed navigate function to anchor tag + re-added space to crypto btn.

--- src/components/Crypto/Donation.tsx
+++ src/components/Crypto/Donation.tsx
@@ -17,10 +17,13 @@ export const CryptoDonationButton = () => {
       <div className="fixed bottom-0 z-10">
         <Button
           type="button"
-          className="donateButton flex items-center rounded-mdml-2 mb-2 px-4 py-2 font-medium  shadow-sm focus:outline-none"
+          className="donateButton flex items-center rounded-mdml-2 mb-2 px-4 py-2 ml-2 font-medium  shadow-sm focus:outline-none"
           onClick={() => setIsCryptoModalOpen(true)}
           variant="bottomButton"
         >
+
+
+
           Donate
           <FaEthereum />
         </Button>
