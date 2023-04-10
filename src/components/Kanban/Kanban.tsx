d51f39ae71a7a9762203ebecf0ae121aab0474b9
trobonox <57040351+trobonox@users.noreply.github.com>
2023-04-10 22:15:52 +0200

    Finish task edit & refactor

--- src/components/Kanban/Kanban.tsx
+++ src/components/Kanban/Kanban.tsx
@@ -2,13 +2,92 @@ import "@Components/Kanban/kanban.css";
 import { useKanban, useToggleKanban } from "@Root/src/store";
 import { IconContext } from "react-icons";
 import { IoCloseSharp } from "react-icons/io5";
+import { BsPlus, BsCheck } from "react-icons/bs";
+import { RxPencil2, RxCheck } from "react-icons/rx";
+import { BiCheck } from "react-icons/bi"
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { FormEvent, useState } from "react";
-import { BsPlus } from "react-icons/bs";
+
 import { v4 } from "uuid";
 import useMediaQuery from "@Utils/hooks/useMediaQuery";
 
-const KanbanColumn = ({ column, addTask, deleteTask }) => {
+const KanbanCard = ({ provided, taskIndex, task, deleteTask, updateTaskName }) => {
+  const [cardEditMode, setCardEditMode] = useState(false);
+  const [cardInputValue, setCardInputValue] = useState(task.name);
+
+  const enableTaskEdit = () => {
+    setCardInputValue(task.name);
+    setCardEditMode(true);
+  }
+
+  const onFormSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    setTaskName();
+  }
+
+  const setTaskName = () => {
+    setCardEditMode(false);
+
+    if (cardInputValue === "" || !/\S/.test(cardInputValue)) {
+      setCardInputValue("");
+      return;
+    }
+
+    updateTaskName(taskIndex, cardInputValue);
+  }
+
+  return (
+    <Draggable key={task.id} draggableId={task.id} index={taskIndex}>
+      {provided => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          style={{
+            ...provided.draggableProps.style,
+            userSelect: "none",
+          }}
+          className="flex flex-grow-0 flex-row items-center justify-between rounded-md bg-gray-300 py-2 pl-2 pr-0.5 dark:bg-gray-600"
+        >
+          {!cardEditMode ? (
+            <span className="text-no-overflow align-middle">{task.name}</span>
+          ) : (
+            <form onSubmit={e => onFormSubmit(e)}>
+              <input
+                className="w-full text-gray-800"
+                autoFocus
+                value={cardInputValue}
+                onChange={event => {
+                  setCardInputValue(event.target.value);
+                }}
+              />
+            </form>
+          )}
+          <div className="flex grow-0 flex-row items-center">
+            {!cardEditMode ? (
+              <RxPencil2
+                onClick={() => enableTaskEdit()}
+                className="h-6 w-6 grow-0 cursor-pointer rounded-md py-1 px-0 text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-500"
+              />
+            ) : (
+              <RxCheck
+                onClick={() => setTaskName()}
+                className="h-6 w-6 grow-0 cursor-pointer rounded-md py-1 px-0 text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-500"
+              />
+            )}
+            <IoCloseSharp
+              onClick={() => deleteTask(taskIndex)}
+              className="h-6 w-6 grow-0 cursor-pointer rounded-md py-1 px-0 text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-500"
+            />
+          </div>
+        </div>
+      )}
+    </Draggable>
+  );
+}
+
+const KanbanColumn = ({ column, addTask, deleteTask, updateTaskName }) => {
   const [taskAddMode, setTaskAddMode] = useState(false);
   const [taskInputValue, setTaskInputValue] = useState("");
 
@@ -30,42 +109,25 @@ const KanbanColumn = ({ column, addTask, deleteTask }) => {
       {provided => {
         return (
           <div ref={provided.innerRef} {...provided.droppableProps} className="w-full">
-            <div className="flex h-64 w-full max-w-[200px] flex-col gap-2 overflow-auto rounded-md border border-gray-200 p-2 dark:border-gray-700 flex-grow-0">
+            <div className="flex h-64 w-full max-w-[200px] flex-grow-0 flex-col gap-2 overflow-auto rounded-md border border-gray-200 p-2 dark:border-gray-700">
               <h2 className="font-bold">{column.title}</h2>
-              <div className="flex flex-col gap-2 overflow-y-auto justify-between h-full">
+              <div className="flex h-full flex-col justify-between gap-2 overflow-y-auto">
                 <div className="flex flex-col gap-2">
-                  {column.tasks.map((task, index) => {
-                    return (
-                      <Draggable key={task.id} draggableId={task.id} index={index}>
-                        {provided => (
-                          <div
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            style={{
-                              ...provided.draggableProps.style,
-                              userSelect: "none",
-                            }}
-                            className="flex flex-grow-0 flex-row items-center justify-between rounded-md bg-gray-300 py-2 pl-2 pr-1 dark:bg-gray-600"
-                          >
-                            <span className="text-no-overflow align-middle">{task.name}</span>
-                            <div className="grow-0">
-                              <IoCloseSharp
-                                onClick={() => deleteTask(index)}
-                                className="h-6 w-6 grow-0 cursor-pointer rounded-md px-1 text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-500"
-                              />
-                            </div>
-                          </div>
-                        )}
-                      </Draggable>
-                    );
-                  })}
+                  {column.tasks.map((task, index) => (
+                    <KanbanCard
+                      task={task}
+                      taskIndex={index}
+                      provided={provided}
+                      deleteTask={taskIndex => deleteTask(taskIndex)}
+                      updateTaskName={(taskIndex, name) => updateTaskName(taskIndex, name)}
+                    />
+                  ))}
                   {provided.placeholder}
                 </div>
                 <div className="w-full">
                   {!taskAddMode ? (
                     <button
-                      className="flex flex-row items-center gap-1 rounded-md px-1 text-left hover:bg-gray-200 dark:hover:bg-gray-600 w-full"
+                      className="flex w-full flex-row items-center gap-1 rounded-md px-1 text-left hover:bg-gray-200 dark:hover:bg-gray-600"
                       onClick={() => setTaskAddMode(true)}
                     >
                       <BsPlus className="h-6 w-6" />
@@ -110,33 +172,28 @@ export const Kanban = ({}) => {
   const { board, setColumns } = useKanban();
   const isDesktop = useMediaQuery("(min-width: 641px)");
 
-  const addTask = (columnId: string, taskName: string) => {
-    const column = board.columns.filter(obj => {
-      return obj.id === columnId;
-    })[0];
-    const columnIndex = board.columns.indexOf(column);
-
+  const addTask = (columnIndex: number, taskName: string) => {
     let columns = board.columns;
     columns[columnIndex].tasks.push({ id: v4(), name: taskName });
 
     setColumns(columns);
   };
 
-  const deleteTask = (taskIndex: number, columnId: string) => {
-    const column = board.columns.filter(obj => {
-      return obj.id === columnId;
-    })[0];
-    const columnIndex = board.columns.indexOf(column);
-
+  const deleteTask = (columnIndex: number, taskIndex: number) => {
     let columns = board.columns;
 
-    if (!confirm(`Are you sure you want to delete the task "${columns[columnIndex].tasks[taskIndex].name}"?`)) {
+    if (!confirm(`Are you sure you want to delete the task "${columns[columnIndex].tasks[taskIndex].name}"?`))
       return;
-    }
 
     columns[columnIndex].tasks.splice(taskIndex, 1);
     setColumns(columns);
   };
+
+  const updateTaskName = (columnIndex: number, taskIndex: number, name: string) => {
+    let columns = board.columns;
+    columns[columnIndex].tasks[taskIndex].name = name;
+    setColumns(columns);
+  }
 
   const onDragEnd = result => {
     const { destination, source, draggableId } = result;
@@ -175,12 +232,13 @@ export const Kanban = ({}) => {
         <div className="cancelDrag flex h-full w-full flex-row items-center gap-2">
           <DragDropContext onDragEnd={onDragEnd}>
             <div className={`flex w-full gap-2 ${isDesktop ? "flex-row" : "flex-col"}`}>
-              {board.columns.map(column => (
+              {board.columns.map((column, columnIndex) => (
                 <KanbanColumn
                   key={column.id}
                   column={column}
-                  addTask={(columnId, taskName) => addTask(columnId, taskName)}
-                  deleteTask={passedIndex => deleteTask(passedIndex, column.id)}
+                  addTask={(taskName) => addTask(columnIndex, taskName)}
+                  deleteTask={passedIndex => deleteTask(columnIndex, passedIndex)}
+                  updateTaskName={(taskIndex, name) => updateTaskName(columnIndex, taskIndex, name)}
                 />
               ))}
             </div>
