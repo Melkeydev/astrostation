f8696605e1d537ec54b1927113b7536e5b78ef1a
Paul Nguon <herropaul@gmail.com>
2023-04-12 12:40:02 -0700

    added quotes surrounding task description

--- src/components/TaskTracker/DeleteModal.tsx
+++ src/components/TaskTracker/DeleteModal.tsx
@@ -3,7 +3,9 @@ export const DeleteModal = ({ task, show, onCancel, onConfirm }) => {
     <div className="modal">
       <div className="max-w-xs rounded-lg bg-white p-2 px-1 text-gray-800 shadow-md dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300">
         <div className="border-gray-100 pb-2">
-          <div className="rounded pb-2 text-center font-bold">Are you sure you want to delete {task.description}?</div>
+          <div className="rounded pb-2 text-center font-bold">
+            Are you sure you want to delete "{task.description}"?
+          </div>
           <hr className="border-t-3 mx-auto w-1/4 border-[#5c5c5c]" />
           <div className=" mt-3 flex justify-center">
             <button onClick={onConfirm} className="mx-2 rounded bg-red-500 px-1 py-1 text-white ">
