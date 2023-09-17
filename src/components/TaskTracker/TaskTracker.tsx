2cd1573e690e1de7f3e09ca6f8bad08bd03f0b39
zemzelett <zemzelett@minoklu.com>
2023-09-18 00:30:26 +0200

    fix: task tracker causes overflow #244

--- src/components/TaskTracker/TaskTracker.tsx
+++ src/components/TaskTracker/TaskTracker.tsx
@@ -13,7 +13,7 @@ export const TaskTracker = () => {
   const [isTaskInfoModalOpen, setIsTaskInfoModalOpen] = useState(false);
 
   return (
-    <div className="rounded-lg border border-gray-200 bg-white/[.96] shadow-md dark:border-gray-700 dark:bg-gray-800/[.96]">
+    <div className="mb-2 w-72 sm:w-96 rounded-lg border border-gray-200 bg-white/[.96] shadow-md dark:border-gray-700 dark:bg-gray-800/[.96]">
       <div className="handle flex w-full cursor-move justify-between p-2">
         <TaskInfoModal isVisible={isTaskInfoModalOpen} onClose={() => setIsTaskInfoModalOpen(false)} />
         <IoInformationCircleOutline
@@ -25,7 +25,7 @@ export const TaskTracker = () => {
           onClick={() => setIsTasksToggled(false)}
         />
       </div>
-      <div className="joyRideTaskTracker mb-2 w-72 pb-3 pr-3 pl-3 dark:text-gray-300 sm:w-96 ">
+      <div className="joyRideTaskTracker pb-3 pr-3 pl-3 dark:text-gray-300">
         <Header title="Task Tracker" onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
         {showAddTask && <AddTask />}
         {tasks.length > 0 ? <Tasks tasks={tasks} /> : "No Tasks to Show"}
