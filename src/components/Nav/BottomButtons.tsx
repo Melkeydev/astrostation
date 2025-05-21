bd4236d3e79d0d4af7d81689ed434d9dcf5a06c6
Vinay Kumar <itsvinay.in@gmail.com>
2025-05-21 19:23:09 +0530

    feature add: export notes locally

--- src/components/Nav/BottomButtons.tsx
+++ src/components/Nav/BottomButtons.tsx
@@ -2,18 +2,44 @@ import { Button } from "../Common/Button";
 import { AiOutlineGithub } from "react-icons/ai";
 import { WithTooltip } from "../Tooltip";
 import { SiGithub } from "react-icons/si";
+import { useStickyNote } from "@Root/src/store";
+import { IStickyNote } from "@Root/src/interfaces";
+
+
+function getNotes(notes: IStickyNote[]): string {
+  const stickyobjs = new Blob([JSON.stringify({ notes })], {
+    type: "application/json"
+  })
+  return URL.createObjectURL(stickyobjs)
+}
+
 
 function BottomButtons() {
+  const { stickyNotes } = useStickyNote()
+  const notesurl = getNotes(stickyNotes)
+
   return (
-    <WithTooltip text="Astrostation is an open source project and is open to contributions.">
-      <div className="fixed bottom-2 z-10 ml-32">
-        <a href="https://github.com/MelkeyOSS/astrostation" target="_blank">
-          <Button className="flex h-10 w-14 items-center rounded-md" variant="bottomButton">
+    <div className="fixed bottom-2 z-10 flex items-center space-x-4 ml-32">
+      <WithTooltip text="Astrostation is an open source project and is open to contributions.">
+        <a href="https://github.com/MelkeyOSS/astrostation" target="_blank" className="inline-flex">
+          <Button className="flex h-10 w-14 items-center justify-center rounded-md" variant="bottomButton">
             <SiGithub size={23} />
           </Button>
         </a>
-      </div>
-    </WithTooltip>
+      </WithTooltip>
+
+      <WithTooltip text="Export Notes Locally">
+        <a
+          href={notesurl}
+          download="notes.json"
+          className="inline-flex"
+        >
+          <Button className="h-10 px-4 flex items-center justify-center" variant="bottomButton">
+            Export
+          </Button>
+        </a>
+      </WithTooltip>
+    </div>
   );
 }
 export default BottomButtons;
